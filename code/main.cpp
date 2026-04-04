5cf0309c896189b94109cb4de290134ae422c3a4
tofimega <fintaistvan05@gmail.com>
2026-04-04 12:02:43 +0200

    basic framework

--- code/main.cpp
+++ code/main.cpp
@@ -1,18 +1,45 @@
 #include <iostream>
 #include <vector>
+#include <map>
 #include <string>
-#include <atomic>
+
 #include "nanobench.h"
+
+#define keytype uint64_t
 
 using namespace std;
 
 
+struct Node {
+    keytype key;
+    Node* left = nullptr;
+    Node* right = nullptr;
+};
+
+class BinTree {
+    Node* root;
+
+    void Insert(keytype key){}
+
+public:
+    BinTree(vector<keytype> keys){}
+    Node Search(){}
+
+};
+
+class BinTreeCont {
+    Node* root;
+    vector<Node> data;
+    void Insert(keytype key){}
+
+public:
+    BinTreeCont(vector<keytype> keys){}
+    Node Search(){}
+
+};
+
 
 int main() {
-    int y = 0;
-    std::atomic<int> x(0);
-    ankerl::nanobench::Bench().run("compare_exchange_strong", [&] {
-        x.compare_exchange_strong(y, 0);
-    });
+
 }
 
