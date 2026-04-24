997b51df19901b5a94e13acd391aa9444ace1ba1
tofimega <fintaistvan05@gmail.com>
2026-04-24 18:44:25 +0200

    fix behavior

--- code/main.cpp
+++ code/main.cpp
@@ -14,25 +14,46 @@ struct Node {
     keytype key;
     Node* left = nullptr;
     Node* right = nullptr;
+
+    Node (){}
+    Node(keytype k) {key = k;}
 };
 
 class BinTree {
     Node* root = nullptr;
 
     void Insert(keytype key){
-        Node*& current = root;
+        if (root == nullptr){
+                root = new Node();
+                root -> key = key;
+                return;
+        }
 
+        Node * parent;
+        Node* current = root;
+        bool left = false;
         while (true)
         {
-            if (current == nullptr){
+            parent = current;
+            if (key < current -> key){
+                 current = current ->left;
+                 left = true;
+            }
+            else if (key > current -> key){
+                current = current -> right;
+                left = false;
+            }
+            else return;
+
+        if (current == nullptr){
                 current = new Node();
                 current -> key = key;
+                if (left) parent -> left = current;
+                else parent -> right = current;
                 return;
             }
 
-            if (key < current -> key) current = current ->left;
-            else if (key > current -> key) current = current -> right;
-            else return;
+
         }
     }
 
@@ -40,40 +61,71 @@ public:
     BinTree(vector<keytype> keys){
         for (keytype k : keys) Insert(k);
     }
-    Node* Search(keytype key){ return nullptr;}
+    Node* Search(keytype key){
+        Node* rt = root;
+
+        while (rt != nullptr && rt->key != key){
+            if (key < rt->key) rt = rt -> left;
+            else rt = rt->right;
+        }
+         return rt;
+        }
 
 };
 
 class BinTreeCont {
-    Node* root = nullptr;
     vector<Node> data;
-    void Insert(keytype key){
-        Node*& current = root;
 
+    void Insert(keytype key){
+        if (data.empty()){
+                data.push_back(Node(key));
+                return;
+        }
+
+        Node * parent;
+        Node* current = &data[0];
+        bool left = false;
         while (true)
         {
-            if (current == nullptr){
-                Node n;
-                n.key = key;
-                data.push_back(n);
-                current = &data[data.size()-1];
+            parent = current;
+            if (key < current -> key){
+                 current = current ->left;
+                 left = true;
+            }
+            else if (key > current -> key){
+                current = current -> right;
+                left = false;
+            }
+            else return;
+
+        if (current == nullptr){
+                data.push_back(Node(key));
+                if (left) parent -> left = &data[data.size()-1];
+                else parent -> right = &data[data.size()-1];
                 return;
             }
 
-            if (key < current -> key) current = current ->left;
-            else if (key > current -> key) current = current -> right;
-            else return;
+
         }
     }
+
 
 public:
     BinTreeCont(vector<keytype> keys){
         data.reserve(keys.size());
         for (keytype k : keys) Insert(k);
     }
-    Node* Search(keytype key){return nullptr;}
 
-};
+    Node* Search(keytype key){
+        Node* rt = &data[0];
+        while (rt != nullptr && rt->key != key){
+            if (key < rt->key) rt = rt -> left;
+            else rt = rt->right;
+        }
+         return rt;
+        }
+    };
+
 
 
 void test(size_t nodes, uint64_t gen_seed, uint64_t test_seed){
@@ -87,7 +139,7 @@ void test(size_t nodes, uint64_t gen_seed, uint64_t test_seed){
     gen_rand.shuffle(keys);
 
     map<uint64_t, Node> map_data;
-    for (keytype k : keys) map_data.insert(std::pair(k, Node()));
+    for (keytype k : keys) map_data.insert(std::pair(k, Node(k)));
 
     BinTree tree_data(keys);
     BinTreeCont cont_data(keys);
@@ -98,31 +150,37 @@ void test(size_t nodes, uint64_t gen_seed, uint64_t test_seed){
 
 
     Node* res;
+    keytype search;
     ankerl::nanobench::Bench bench = ankerl::nanobench::Bench().title("Search Time Benchmark").unit("nodes").batch(nodes).name("map test").run(
         [&]{
-            ankerl::nanobench::doNotOptimizeAway(res = &map_data[test_rand_map()%nodes]);
+            search = test_rand_map()%nodes;
+            ankerl::nanobench::doNotOptimizeAway(res = &map_data[search]);
         });
 
-        cout << "Map test finished, last node key: "  << ((res == nullptr) ? -1 : res->key) << "\n";
+        cout << "Map test finished, last search key: " << search << " last node key: "  << ((res == nullptr) ? -1 : res->key) << "\n";
   ankerl::nanobench::Rng test_rand_tree(test_seed);
 
-        bench.name("simple bintree test").run(
+        bench.name("simple bintree test").run( 
         [&]{
-             ankerl::nanobench::doNotOptimizeAway(res = tree_data.Search(test_rand_tree()%nodes));
+            search = test_rand_tree()%nodes;
+             ankerl::nanobench::doNotOptimizeAway(res = tree_data.Search(search));
         });
 
-        cout << "Simple binary tree test finished, last node key: "  << ((res == nullptr) ? -1 : res->key)<< "\n";
+        cout << "Simple binary tree test finished, last search key: " << search << " last node key: "  << ((res == nullptr) ? -1 : res->key) << "\n";
+
+
     ankerl::nanobench::Rng test_rand_cont(test_seed);
         bench.name("continuous bintree test").run(
         [&]{
-             ankerl::nanobench::doNotOptimizeAway(res = tree_data.Search(test_rand_tree()%nodes));
+            search = test_rand_cont()%nodes;
+             ankerl::nanobench::doNotOptimizeAway(res = tree_data.Search(search));
         });
 
-        cout << "Continuous binary tree test finished, last node key: "  << ((res == nullptr) ? -1 : res->key)<< "\n";
+        cout << "Continuous binary tree test finished, last search key: " << search << " last node key: "  << ((res == nullptr) ? -1 : res->key) << "\n";
 
 
-        vector<ankerl::nanobench::Result> results = bench.results();
-        bench.render(ankerl::nanobench::templates::json(), cout);
+       // vector<ankerl::nanobench::Result> results = bench.results();
+       // bench.render(ankerl::nanobench::templates::json(), cout);
 }
 
 
